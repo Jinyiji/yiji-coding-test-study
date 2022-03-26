095a621f0bade4564aef2f347cd64ae45743a0de
Yiji Jin <73942023+Jinyiji@users.noreply.github.com>
2022-03-26 23:26:46 +0900

    Test (#2)

--- src/progarmmers/test3416.java
+++ src/progarmmers/test3416.java
@@ -2,6 +2,6 @@ package progarmmers;
 
 public class test3416 {
     public static void main(String[] args) {
-        System.out.println("테스트 출력 예시");
+        // System.out.println("테스트 출력 예시");
     }
 }
